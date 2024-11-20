bb502b544f87863e57d0a7b742976c85788ab44d
fifthbarrier <nudnouil@gmail.com>
2024-11-20 17:42:14 -0600

    reversing y coord

--- scripts/main.js
+++ scripts/main.js
@@ -777,7 +777,7 @@ function generateLookupArrays() {
 		/* manually moved systems, usually oorp/removed ones */
 		/* 0,0 in upper left corner, first coordinate is x and second is y */
 		systemPosArray["li09"] = [7,9.25];
-		systemPosArray["ew15"] = [3.45,20];
+		systemPosArray["ew15"] = [3.45,1];
 
 		systemIdsNameArrayState = "ready";
 		systemScaleFactorArrayState = "ready";
