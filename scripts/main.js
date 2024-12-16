8652e30df07a0d1c026188f4315a158069913142
Barrier <nudnouil@gmail.com>
2024-12-16 13:55:31 -0600

    moving Burgundy

--- scripts/main.js
+++ scripts/main.js
@@ -777,7 +777,7 @@ function generateLookupArrays() {
 		/* manually moved systems, usually oorp/removed ones */
 		/* 0,0 in upper left corner, first coordinate is x and second is y */
 		systemPosArray["li09"] = [7,9.25];
-		systemPosArray["ew15"] = [2.5,0.04];
+		systemPosArray["ew15"] = [2,0.01];
 
 		systemIdsNameArrayState = "ready";
 		systemScaleFactorArrayState = "ready";
